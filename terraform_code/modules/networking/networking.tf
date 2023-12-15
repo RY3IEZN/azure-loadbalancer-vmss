09fd7e0ca35537bdd4bb69b8e4923e8ddab60986
unknown <unekue2@gmail.com>
2023-12-15 08:29:03 +0100

    added NSG rule

--- terraform_code/modules/networking/networking.tf
+++ terraform_code/modules/networking/networking.tf
@@ -21,4 +21,29 @@ resource "azurerm_subnet" "az_alb_vm_public_subnet" {
   address_prefixes     = [var.subnet_address_space]
 }
 
+resource "azurerm_network_security_group" "az_alb_nsg" {
+  name                = "acceptanceTestSecurityGroup1"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
 
+  security_rule {
+    name                       = "nginx"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "80"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = var.subnet_address_space
+  }
+
+  tags = {
+    uneku = "nsg"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "az_nsg_public_subnet_assoc" {
+  subnet_id                 = azurerm_subnet.az_alb_vm_public_subnet.id
+  network_security_group_id = azurerm_network_security_group.az_alb_nsg.id
+}
